8fe583bdce7eb63fdc8fc3ac83972505efd9f202
sentayhuJo <sentayhu.alim@mail.com>
2017-08-05 11:31:34 +0100

    code reduction

--- server.js
+++ server.js
@@ -1,30 +1,23 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const methodOverride = require('method-override');
 const app = express();
 const config = require('./app/config/config');
 
-app.use(morgan('dev'));                                   // log every request to the console
-app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());                                     // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-app.use(methodOverride());
-
-//Connecting MongoDB using mongoose to our application
+//Connecting MongoDB using mongoose to the application
 mongoose.connect(config.db);
 
 //This callback will be triggered once the connection is successfully established to MongoDB
-mongoose.connection.on('connected', function () {
+mongoose.connection.on('connected',() => {
   console.log('Mongoose default connection open to ' + config.db);
 });
-app.get('/', function(req, res){
+
+app.get('/', (req, res) => {
   res.send('hello');
 })
-//Express application will listen to port mentioned in our configuration
-app.listen(config.port, function(err){
+
+//Express application will listen to port mentioned in the configuration
+app.listen(config.port,err => {
   if(err) throw err;
   console.log("App listening on port " + config.port);
 });
---
+++ test/event-api.spec.js
@@ -0,0 +1,12 @@
+const sinon = require('sinon');
+const chai = require('chai');
+const expect = chai.expect;
+
+const mongoose = require('mongoose');
+require('sinon-mongoose');
+
+//Importing our todo model for our unit testing.
+const eventSchema = require('../app/model/event-schema');
+describe('', () => {
+
+});
